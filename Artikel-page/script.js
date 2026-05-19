d795da46cb327d2acea07c07971379f98349647d
Neo <neogoldahlgren@gmail.com>
2026-05-19 11:43:20 +0200

    ny artikel och knapp för sig

---
+++ Artikel-page/script.js
@@ -0,0 +1,5 @@
+let date = new Date()
+let year = date.getFullYear(); // Hämtar året
+let month = String(date.getMonth() + 1).padStart(2, '0'); // Hämtar månaden (0-indexerad, +1) och lägger till en ledande nolla
+let day = String(date.getDate()).padStart(2, '0'); // Hämtar dagen och lägger till en ledande nolla
+let formattedDate = `${year}-${month}-${day}`; // Formaterar till xxxx-xx-xx
